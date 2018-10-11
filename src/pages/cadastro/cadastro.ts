61cf1e79ac4d2a2317da4978c8b81dfae691c05f
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2018-10-11 20:34:42 -0300

    validacao de formulario

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -14,23 +14,38 @@ import { Aluno } from './../../models/aluno.model';
 export class CadastroPage {
 
 	formulario = new FormGroup({
+		matricula: new FormControl('', [
+			Validators.required,
+			Validators.pattern('(20)(0|1)([0-8])(1|2)(tinf)([0-9]{4})'),
+			Validators.nullValidator
+		]),
 		nome: new FormControl('', [
 			Validators.required,
 			Validators.minLength(5),
 			Validators.maxLength(50),
+			Validators.nullValidator
 		]),
 		email: new FormControl('', [
 			Validators.required,
-			Validators.pattern('\\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$')
+			Validators.email,
+			Validators.nullValidator
+		]),
+		idade: new FormControl(18, [
+			Validators.required,
+			Validators.nullValidator
+		]),
+		sexo: new FormControl('', [
+			Validators.required,
+			Validators.nullValidator
 		])
 	});
 	alunoASerCadastrado: Aluno;
 
 	cadastrarAlunos() {
 
-		this.formulario.patchValue({
-			nome: "nome do aluno modificado"
-		});
+		console.log(this.alunoASerCadastrado)
+		console.log(this.formulario)
+
 
 		// let toast = this.alertCtrl.create({
 		// 	message: 'Cadastrando Usuário ....',
